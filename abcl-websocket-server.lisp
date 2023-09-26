abc4bf1aceac8ac4d8dcd3c31f32955ac51ad16c
Alejandro Zamora <ale2014.zamora@gmail.com>
2023-09-26 11:13:21 -0300

    added dummy constructor and method onError

--- abcl-websocket-server.lisp
+++ abcl-websocket-server.lisp
@@ -2,12 +2,21 @@
 
 (in-package #:abcl-websocket-server)
 
-(java:jnew-runtime-class
- "ABCLWebsocketServer"
- :superclass "org.java_websocket.server.WebSocketServer"
- :methods (list
-           (list "onOpen" :void '("org.java_websocket.WebSocket" "org.java_websocket.handshake.ClientHandshake")
-                 (lambda (this conn handshake)
-                   (let ((get-remote-socket-addr-method
-                           (java:jmethod "org.java_websocket.WebSocket" "getRemoteSocketAddress")))
-                    (format t "new connection from ~a~%" (java:jcall get-remote-socket-addr-method conn)))))))
+(defparameter *websocket-class*
+    (java:jnew-runtime-class
+     "ABCLWebsocketServer"
+     :constructors (list
+                    (list (list "java.net.InetSocketAddress")
+                          (lambda (this address)
+                            (print this)
+                            (print address))))
+     :superclass "org.java_websocket.server.WebSocketServer"
+     :methods (list
+               (list "onOpen" :void '("org.java_websocket.WebSocket" "org.java_websocket.handshake.ClientHandshake")
+                     (lambda (this conn handshake)
+                       (let ((get-remote-socket-addr-method
+                               (java:jmethod "org.java_websocket.WebSocket" "getRemoteSocketAddress")))
+                         (format t "new connection from ~a~%" (java:jcall get-remote-socket-addr-method conn)))))
+               (list "onError" :void '("org.java_websocket.WebSocket" "java.lang.Exception")
+                     (lambda (this conn exception)
+                       (format t "an exception ocurred ~a~%" exception))))))
