e486c48d577598671108bc4385a138f6079a4f2a
Alejandro Zamora <ale2014.zamora@gmail.com>
2023-09-26 15:45:16 -0300

    initial version! GD!!!
    it only needs to complete some more methods

--- abcl-websocket-server.lisp
+++ abcl-websocket-server.lisp
@@ -9,9 +9,13 @@
                     (list (list "java.net.InetSocketAddress")
                           (lambda (this address)
                             (print this)
-                            (print address))))
+                            (print address))
+                          (list 1)))
      :superclass "org.java_websocket.server.WebSocketServer"
      :methods (list
+               (list "onMessage" :void '("org.java_websocket.WebSocket" "java.nio.ByteBuffer")
+                     (lambda (this message)
+                       (format t "var message=~a~%" message)))
                (list "onOpen" :void '("org.java_websocket.WebSocket" "org.java_websocket.handshake.ClientHandshake")
                      (lambda (this conn handshake)
                        (let ((get-remote-socket-addr-method
@@ -19,4 +23,7 @@
                          (format t "new connection from ~a~%" (java:jcall get-remote-socket-addr-method conn)))))
                (list "onError" :void '("org.java_websocket.WebSocket" "java.lang.Exception")
                      (lambda (this conn exception)
-                       (format t "an exception ocurred ~a~%" exception))))))
+                       (format t "an exception ocurred ~a~%" exception)))
+               (list "onStart" :void '()
+                     (lambda (this)
+                       (print "starting..."))))))
