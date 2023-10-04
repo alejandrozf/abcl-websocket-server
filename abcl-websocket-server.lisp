254ce58ae2d7d93816094530f175ccf5fec93c59
Alejandro Zamora <ale2014.zamora@gmail.com>
2023-10-04 13:59:48 -0300

    added static handler! GD!!!

--- abcl-websocket-server.lisp
+++ abcl-websocket-server.lisp
@@ -26,32 +26,35 @@
                  "send" "java.lang.String") connection message))
 
 
-(defmacro make-ws-server (path port &key on-open on-message on-ping on-pong on-close)
+(defmacro make-ws-server (path port &key on-open on-message on-ping on-pong on-close (static-path "/tmp"))
   `(java:jcall
     (java:jmethod "org.webbitserver.netty.NettyWebServer" "start")
     (java:jcall
-     (java:jmethod "org.webbitserver.netty.NettyWebServer"
-                   "add"
-                   "java.lang.String"
-                   "org.webbitserver.WebSocketHandler")
-     (add-new-server
-      (java:jstatic (java:jmethod "org.webbitserver.WebServers" "createWebServer" "int")
-                    "org.webbitserver.WebServers" ,port))
-     ,path
-     (java:jinterface-implementation
-      "org.webbitserver.WebSocketHandler"
-      "onOpen" (lambda (connection)
-                 (declare (ignorable connection))
-                 (progn ,@on-open))
-      "onClose" (lambda (connection)
+     (java:jmethod "org.webbitserver.netty.NettyWebServer" "add" "org.webbitserver.HttpHandler")
+     (java:jcall
+      (java:jmethod "org.webbitserver.netty.NettyWebServer"
+                    "add"
+                    "java.lang.String"
+                    "org.webbitserver.WebSocketHandler")
+      (add-new-server
+       (java:jstatic (java:jmethod "org.webbitserver.WebServers" "createWebServer" "int")
+                     "org.webbitserver.WebServers" ,port))
+      ,path
+      (java:jinterface-implementation
+       "org.webbitserver.WebSocketHandler"
+       "onOpen" (lambda (connection)
                   (declare (ignorable connection))
-                  (progn ,@on-close))
-      "onMessage" (lambda (connection message)
-                    (declare (ignorable connection message))
-                    (progn ,@on-message))
-      "onPing" (lambda (connection message)
-                 (declare (ignorable connection message))
-                 (progn ,@on-ping))
-      "onPong" (lambda (connection message)
-                 (declare (ignorable connection message))
-                 (progn ,@on-pong))))))
+                  (progn ,@on-open))
+       "onClose" (lambda (connection)
+                   (declare (ignorable connection))
+                   (progn ,@on-close))
+       "onMessage" (lambda (connection message)
+                     (declare (ignorable connection message))
+                     (progn ,@on-message))
+       "onPing" (lambda (connection message)
+                  (declare (ignorable connection message))
+                  (progn ,@on-ping))
+       "onPong" (lambda (connection message)
+                  (declare (ignorable connection message))
+                  (progn ,@on-pong))))
+     (java:jnew "org.webbitserver.handler.StaticFileHandler" ,static-path))))
